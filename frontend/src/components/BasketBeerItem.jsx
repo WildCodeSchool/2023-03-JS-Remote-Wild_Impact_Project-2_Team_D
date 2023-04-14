d1c8bbd80a88f6d20853696a9be8ace9a8859b62
Bnt-dev <bberanger.pro@gmail.com>
2023-04-14 13:38:01 +0200

    Start of Basket Page with css + components + img

---
+++ frontend/src/components/BasketBeerItem.jsx
@@ -0,0 +1,56 @@
+import "@pages/BasketPage.css";
+import bin from "@assets/bin.png";
+import plus from "@assets/plus.png";
+import moins from "@assets/moins.png";
+// import { useState } from "react";
+
+function BeerBasketCard({ beer }) {
+  // const priceByBottle = beer.price_per_liter * 0.33;
+  // const priceByBottleAround = priceByBottle.toFixed(2);
+
+  // const [addBeer, setAddBeer] = useState(1);
+
+  return (
+    <div className="basketItem">
+      <div className="beerListItem">
+        <img
+          src={beer.image_url}
+          alt="bière {beer.name}"
+          className="imgBeerBasket"
+        />
+        <div className="beerDetailList">
+          <div className="beerDetailItem">
+            <p className="beerBasketName">{beer.name}</p>
+            {/* <p className="beerBasketDetail">Quantité : {addBeer}</p> */}
+            <p className="beerBasketDetail">Volume : 33 cl</p>
+            <div>
+              <p className="beerBasketDetail">
+                Prix au litre : <span>{beer.price_per_liter} €</span>
+              </p>
+            </div>
+          </div>
+          <div className="basketIcons">
+            <div className="basketIcon">
+              {/* onClick={() => setAddBeer(0)} */}
+              <img src={bin} alt="Bin" />
+            </div>
+            <div className="basketIcon">
+              {/* onClick={() => setAddBeer((numberBeer) => numberBeer + 1)} */}
+
+              <img src={plus} alt="Add" />
+            </div>
+            <div className="basketIcon">
+              {/* onClick={() => setAddBeer((numberBeer) => numberBeer - 1)} */}
+              <img src={moins} alt="Less" />
+            </div>
+          </div>
+        </div>
+        <div className="ttcByBeer">
+          {/* <p>Total : {(addBeer * priceByBottleAround).toFixed(2)} €</p> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BeerBasketCard;
