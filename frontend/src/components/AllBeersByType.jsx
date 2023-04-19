4583e8915e2237a20adb926a9eed74901d394c00
NicolasPanis <nicolaspanis12@gmail.com>
2023-04-19 16:05:38 +0200

    Mise en place du filtre par catégorie/type

--- frontend/src/components/AllBeersByType.jsx
+++ frontend/src/components/AllBeersByType.jsx
@@ -7,24 +7,26 @@ function AllBeersByType({ title, cls }) {
     <div className="containerBeer">
       <h2 className="titleHome">{title}</h2>
       <div className={cls}>
-        {beers.map((beer) => (
-          <article key={beer.id}>
-            <div className="headerCard">
-              <button type="button">+</button>
-            </div>
-            <img src={beer.image_url} alt={beer.id} />
-            <div className="footerCard">
-              <button type="button">
-                <p>+ d'infos</p>
-              </button>
-              <p>x12</p>
-            </div>
-            <div className="beerName">
-              <h3>{beer.name}</h3>
-              <h3>10e</h3>
-            </div>
-          </article>
-        ))}
+        {beers
+          .filter((beer) => beer.type === "IPA")
+          .map((beer) => (
+            <article key={beer.id}>
+              <div className="headerCard">
+                <button type="button">+</button>
+              </div>
+              <img src={beer.image_url} alt={beer.id} />
+              <div className="footerCard">
+                <button type="button">
+                  <p>+ d'infos</p>
+                </button>
+                <p>x12</p>
+              </div>
+              <div className="beerName">
+                <h3>{beer.name}</h3>
+                <h3>10e</h3>
+              </div>
+            </article>
+          ))}
       </div>
     </div>
   );
