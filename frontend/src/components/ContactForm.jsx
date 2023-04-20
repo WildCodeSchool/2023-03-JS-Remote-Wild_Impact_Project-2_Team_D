3ca2380fa82ba1eaf6fbdff9309a82f18e194c3d
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-04-20 16:04:23 +0200

    mise en place state

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -13,48 +13,50 @@ function ContactForm() {
 
   return (
     <div className="contactForm" data-theme="light">
-      <h3>Contactez nous</h3>
-      <form action="">
-        <div className="userNameEmail">
-          <div className="userInfo">
-            <input
-              className="inputFieldName"
-              type="text"
-              name="name"
-              value={name}
-              placeholder="Nom et Prénom"
-              onChange={(e) => setName(e.target.value)}
-            />
+      <div className="formContainer">
+        <h3>Contactez nous</h3>
+        <form action="">
+          <div className="userNameEmail">
+            <div className="userInfo">
+              <input
+                className="inputFieldName"
+                type="text"
+                name="name"
+                value={name}
+                placeholder="Nom"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="userInfo">
+              <input
+                className="inputFieldEmail"
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
           </div>
           <div className="userInfo">
-            <input
-              className="inputFieldEmail"
-              type="email"
-              name="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+            <textarea
+              className="inputField"
+              name="message"
+              id="message"
+              cols="30"
+              rows="4"
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             />
           </div>
-        </div>
-        <div className="userInfo">
-          <textarea
-            className="inputField"
-            name="message"
-            id="message"
-            cols="30"
-            rows="4"
-            placeholder="Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </div>
-        <div className="divSubmitButton">
-          <button type="button" className="submitButton" onClick={onSubmit}>
-            Envoyer
-          </button>
-        </div>
-      </form>
+          <div className="divSubmitButton">
+            <button type="button" className="submitButton" onClick={onSubmit}>
+              Envoyer
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
