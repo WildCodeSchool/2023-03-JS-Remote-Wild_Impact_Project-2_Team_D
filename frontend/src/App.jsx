06c716d8aac3aa300088dcd9aa694a3e62d1bbf3
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2023-04-21 11:18:28 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-
 // import Home from "./pages/Home";
-
 import "@picocss/pico";
 // US 1 import
 
 // US 2 Import
-import Page2 from "./pages/Page2";
+import Page2 from "@pages/Page2";
+
 // US 3 import
 import CartPage from "@pages/Cart";
-// US 4 import
 
+// US 4 import
 // ContactForm import
 import ContactForm from "@components/ContactForm";
-
 import "./App.css";
 
 function App() {
