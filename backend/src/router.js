c2e0eeddb5eb8777fd08a273bb02f03e335a5447
NicolasPanis <nicolaspanis12@gmail.com>
2023-04-26 16:00:30 +0200

    Router allbeer, byid, bytype

--- backend/src/router.js
+++ backend/src/router.js
@@ -12,11 +12,45 @@ connexion
     );
   })
   .catch((err) => console.error(err));
-// const itemControllers = require("./controllers/itemControllers");
-// router.get("/items", itemControllers.browse);
-// router.get("/items/:id", itemControllers.read);
-// router.put("/items/:id", itemControllers.edit);
-// router.post("/items", itemControllers.add);
-// router.delete("/items/:id", itemControllers.destroy);
+
+const welcome = (req, res) => {
+  res.send("Hello");
+};
+
+const getBeers = (req, res) => {
+  let url = "SELECT * FROM beers";
+  const value = [];
+  if (req.query.type) {
+    url += " WHERE type = ?";
+    value.push(req.query.type);
+  }
+  connexion
+    .query(url, value)
+    .then(([beers]) => {
+      res.json(beers);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getBeersById = (req, res) => {
+  const { id } = req.params;
+
+  connexion
+    .query("SELECT * FROM beers WHERE id = ?", [id])
+    .then(([beers]) => {
+      res.json(beers[0]);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+router.get("/", welcome);
+router.get("/beers", getBeers);
+router.get("/beers/:id", getBeersById);
 
 module.exports = router;
