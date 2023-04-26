f5ac852561123a7ae47e45dd5a496d25ceaab966
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-04-26 10:58:32 +0200

    create table

--- backend/database.sql
+++ backend/database.sql
@@ -1,6 +1,16 @@
-CREATE TABLE item (
-  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  title varchar(255) NOT NULL
+-- SQLBook: Code
+CREATE TABLE beers (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
+  tagline varchar(255) NOT NULL,
+  first_brewed varchar(255) NOT NULL,
+  description varchar(500) NOT NULL,
+  image_url varchar(255) NOT NULL,  
+  price_per_liter INT NOT NULL,
+  abv INT NOT NULL,
+  ph INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+
