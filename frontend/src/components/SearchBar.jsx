5a822bde1b4974d8be67d9e4d035732035a8ce11
NicolasPanis <nicolaspanis12@gmail.com>
2023-05-03 15:19:17 +0200

    Mise en place du usestate et useeffect

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "@components/SearchBar.css";
 
-function SearchBar() {
-  return <div>SearchBar</div>;
+function searchBar() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5500/beers`)
+      .then((response) => response.json())
+      .then((json) => setBeers(json))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const handleSearch = () => {};
+
+  return (
+    <div className="searchBarContainer">
+      <div className="searchBar">
+        <input
+          type="text"
+          // value={beers}
+          name="searchBar"
+          id="searchBar"
+          placeholder="Rechercher"
+          onChange={handleSearch}
+        />
+      </div>
+      <ul className="results">
+        {beers.slice(0, 5).map((beer) => (
+          <li className="result" key={beer.id}>
+            <h4>{beer.name}</h4>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default SearchBar;
+export default searchBar;
