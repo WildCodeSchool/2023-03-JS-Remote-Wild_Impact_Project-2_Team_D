05c4c1f388bdeef2c6796ae776ee72c636a8e2b2
NicolasPanis <nicolaspanis12@gmail.com>
2023-05-03 15:50:06 +0200

    Mise en place du Useeffect avec la condition if et le deuxième useState

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,30 +3,32 @@ import "@components/SearchBar.css";
 
 function searchBar() {
   const [beers, setBeers] = useState([]);
-
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    fetch(`http://localhost:5500/beers`)
-      .then((response) => response.json())
-      .then((json) => setBeers(json))
-      .catch((err) => console.error(err));
-  }, []);
-
-  const handleSearch = () => {};
+    if (search !== "") {
+      fetch(`http://localhost:5500/beers?name=${search}`)
+        .then((response) => response.json())
+        .then((json) => setBeers(json))
+        .catch((err) => console.error(err));
+    } else {
+      setBeers([]);
+    }
+  }, [search]);
 
   return (
     <div className="searchBarContainer">
       <div className="searchBar">
         <input
           type="text"
-          // value={beers}
+          value={search}
           name="searchBar"
           id="searchBar"
           placeholder="Rechercher"
-          onChange={handleSearch}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </div>
       <ul className="results">
-        {beers.slice(0, 5).map((beer) => (
+        {beers.map((beer) => (
           <li className="result" key={beer.id}>
             <h4>{beer.name}</h4>
           </li>
