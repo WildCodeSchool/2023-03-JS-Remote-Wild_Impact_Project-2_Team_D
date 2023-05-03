72cf005c115bbaca9d66fae4906c8e44c9a18f0c
Adrien Turcek-Perron <adrien141094@gmail.com>
2023-05-03 17:10:22 +0200

    maj route onglert home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import Carrousel from "@components/Carrousel";
 import AllBeersByType from "@components/AllBeersByType";
 import About from "@components/About";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = `Aromalt`;
+  }, []);
   return (
     <header className="App-header">
       <Carrousel />
