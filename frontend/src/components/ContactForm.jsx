894d03ad516e3224c37915530fd277648a2ae81f
FLORIANE BERTHIER <floriane.berthier96@gmail.com>
2023-05-05 09:30:03 +0200

    ajout formulaire de contact connecté a emailjs

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { ToastContainer, toast } from "react-toastify";
 // eslint-disable-next-line import/no-extraneous-dependencies
+import emailjs from "@emailjs/browser";
 import "react-toastify/dist/ReactToastify.css";
 import "./ContactForm.css";
 
@@ -9,16 +10,32 @@ function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const form = useRef();
 
-  const notify = () => {
-    toast(`Merci ${name}, Aromalt te répondra prochainement !`);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_at6jcos",
+        "contact_form",
+        form.current,
+        "dP5Z5bT63zWeqUZB1"
+      )
+      .then(() => {
+        toast(`Merci ${name}, Aromalt te répondra prochainement !`);
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-alert
+        alert("il y a un problème");
+      });
   };
 
   return (
     <div className="contactForm" data-theme="light">
       <div className="formContainer">
         <h3>Contactez nous</h3>
-        <form action="">
+        <form className="ContactForm" ref={form} onSubmit={sendEmail}>
           <div className="userNameEmail">
             <div className="userInfo">
               <input
@@ -54,7 +71,7 @@ function ContactForm() {
             />
           </div>
           <div className="divSubmitButton">
-            <button type="button" className="submitButton" onClick={notify}>
+            <button type="submit" className="submitButton">
               Envoyer
             </button>
             <ToastContainer
